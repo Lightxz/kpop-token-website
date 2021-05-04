17ec979ac0f4d2e5093bf8a4d52cf91ab841c917
Mark Galang <kristopher.galang@tooltwist.com>
2021-05-04 23:31:42 +0800

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "styles/App.css";
+
+// Pages
+import HomePage from "pages/Index";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="container">
+        <HomePage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
