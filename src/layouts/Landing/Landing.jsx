151ba322e8d43d766805bd0f9eea7a4764e7f52f
Mark Galang <galangmarkg@gmail.com>
2021-05-09 00:17:10 +0800

    Enhance Styling

--- src/layouts/Landing/Landing.jsx
+++ src/layouts/Landing/Landing.jsx
@@ -1,39 +1,44 @@
 import React from "react";
 import "./Landing.css";
 
+// Components
+import Container from "components/Container/Container";
+
 const Landing = () => {
   return (
     <div id="home" className="landing-container">
-      <div className="landing-body row">
-        <div className="col-lg-9">
-          <div className="row">
-            <div className="col-lg-7">
-              <h1>A Cryptocurrency Fan Token For The KPOP stans</h1>
+      <Container>
+        <div className="landing-body">
+          <div className="col-lg-9">
+            <div className="row">
+              <div className="col-lg-7">
+                <h1>A Cryptocurrency Fan Token For The KPOP stans</h1>
+              </div>
             </div>
+            <p>
+              $KPOP Fan Tokens give you exclusive access to special promotions,
+              VIP events, licensed digital photos and moments from your favorite
+              KPOP idols,and a chance to win a FULL KPOP experience in Korea!
+            </p>
+
+            <p>
+              Be more than a fan, be a superfan! Join a fun-filled community of
+              KPOP stans!
+            </p>
+
+            <a href="#">
+              <button type="button" className="btn buy-now">
+                Buy Now
+              </button>
+            </a>
+            <a href="#">
+              <button type="button" className="btn learn-more">
+                Learn More
+              </button>
+            </a>
           </div>
-          <p>
-            $KPOP Fan Tokens give you exclusive access to special promotions,
-            VIP events, licensed digital photos and moments from your favorite
-            KPOP idols,and a chance to win a FULL KPOP experience in Korea!
-          </p>
-
-          <p>
-            Be more than a fan, be a superfan! Join a fun-filled community of
-            KPOP stans!
-          </p>
-
-          <a href="#">
-            <button type="button" className="btn buy-now">
-              Buy Now
-            </button>
-          </a>
-          <a href="#">
-            <button type="button" className="btn learn-more">
-              Learn More
-            </button>
-          </a>
         </div>
-      </div>
+      </Container>
     </div>
   );
 };
