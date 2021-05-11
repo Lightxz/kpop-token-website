dd9a357af997e433c8b141303e3591fdb464f1b7
gopezella <46973844+gopezella@users.noreply.github.com>
2021-05-11 16:14:52 +0800

    Fan Token Style

--- src/layouts/RoadMap/RoadMap.jsx
+++ src/layouts/RoadMap/RoadMap.jsx
@@ -1,10 +1,72 @@
 import React from "react";
 import "./RoadMap.css";
 
+// Bootstrap
+import { Row, Col, Badge } from "react-bootstrap";
+
 const RoadMap = () => {
   return (
     <div id="roadmap" className="roadmap-container">
-      <h1>ROADMAP</h1>
+      <Row>
+        <Col lg={1} className="space"></Col>
+        <Col lg={3}>
+          <div className="timeline-item">
+            <h3>Title Goes Here</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque lobortis pharetra mauris, et eleifend arcu faucibus
+              quis. Vestibulum leo leo, auctor nec nibh in, eleifend
+              pellentesque eros.
+            </p>
+          </div>
+        </Col>
+
+        <Col lg={3} className="timeline-item end-space"></Col>
+
+        <Col lg={3}>
+          <div className="timeline-item">
+            <h3>Title Goes Here</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque lobortis pharetra mauris, et eleifend arcu faucibus
+              quis. Vestibulum leo leo, auctor nec nibh in, eleifend
+              pellentesque eros.
+            </p>
+          </div>
+        </Col>
+
+        <Col lg={2} className="timeline-item end-space"></Col>
+      </Row>
+
+      <Row>
+        <Col lg={1}></Col>
+        <Col lg={3}></Col>
+        <Col lg={3} className="timelime-item2-col">
+          <div className="timeline-item2">
+            <h3>Title Goes Here</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque lobortis pharetra mauris, et eleifend arcu faucibus
+              quis. Vestibulum leo leo, auctor nec nibh in, eleifend
+              pellentesque eros.
+            </p>
+          </div>
+        </Col>
+
+        <Col lg={3} className="timelime-item2-col"></Col>
+
+        <Col lg={2} className="timelime-item2-col">
+          <div className="timeline-item2">
+            <h3>Title Goes Here</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque lobortis pharetra mauris, et eleifend arcu faucibus
+              quis. Vestibulum leo leo, auctor nec nibh in, eleifend
+              pellentesque eros.
+            </p>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 };
