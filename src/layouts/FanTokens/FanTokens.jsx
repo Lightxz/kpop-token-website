a40c02c7eb405f2a4ae130bbfcc253ab0c81164b
Mark Galang <galangmarkg@gmail.com>
2021-05-11 17:18:47 +0800

    Add Resonsive Fan Token

--- src/layouts/FanTokens/FanTokens.jsx
+++ src/layouts/FanTokens/FanTokens.jsx
@@ -2,10 +2,8 @@ import React from "react";
 import "./FanTokens.css";
 
 //Components
+import Container from "components/Container/Container";
 import FanToken from "components/FanToken/FanToken";
-
-// Bootstrap
-import { Row, Col } from "react-bootstrap";
 
 //Image
 import btsPhoto from "images/BTS-group-photo.png";
@@ -15,15 +13,15 @@ const FanTokens = () => {
   return (
     <div id="fanTokens" className="fan-tokens-container">
       <h1 className="fan-tokens-title">Fan Token</h1>
-      <Row>
-        <Col sm={6} sm={6} lg={3}>
+      <Container>
+        <div className="fan-tokens">
           <div className="fan-token-card">
-            <div className="bts-group-container">
-              <img class="bts-group" src={btsPhoto} />
+            <div className="group-image-container">
+              <img class="group-image" src={btsPhoto} />
             </div>
 
-            <div className="bts">
-              <p className="bts-name">BTS</p>
+            <div className="group-name-container">
+              <p className="group-name">BTS</p>
               <img src={btsLogo} />
             </div>
 
@@ -44,16 +42,13 @@ const FanTokens = () => {
               </div>
             </div>
           </div>
-        </Col>
-
-        <Col sm={6} lg={3}>
           <div className="fan-token-card">
-            <div className="bts-group-container">
-              <img class="bts-group" src={btsPhoto} />
+            <div className="group-image-container">
+              <img class="group-image" src={btsPhoto} />
             </div>
 
-            <div className="bts">
-              <p className="bts-name">BTS</p>
+            <div className="group-name-container">
+              <p className="group-name">BTS</p>
               <img src={btsLogo} />
             </div>
 
@@ -74,16 +69,13 @@ const FanTokens = () => {
               </div>
             </div>
           </div>
-        </Col>
-
-        <Col sm={6} lg={3}>
           <div className="fan-token-card">
-            <div className="bts-group-container">
-              <img class="bts-group" src={btsPhoto} />
+            <div className="group-image-container">
+              <img class="group-image" src={btsPhoto} />
             </div>
 
-            <div className="bts">
-              <p className="bts-name">BTS</p>
+            <div className="group-name-container">
+              <p className="group-name">BTS</p>
               <img src={btsLogo} />
             </div>
 
@@ -104,16 +96,13 @@ const FanTokens = () => {
               </div>
             </div>
           </div>
-        </Col>
-
-        <Col sm={6} lg={3}>
           <div className="fan-token-card">
-            <div className="bts-group-container">
-              <img class="bts-group" src={btsPhoto} />
+            <div className="group-image-container">
+              <img class="group-image" src={btsPhoto} />
             </div>
 
-            <div className="bts">
-              <p className="bts-name">BTS</p>
+            <div className="group-name-container">
+              <p className="group-name">BTS</p>
               <img src={btsLogo} />
             </div>
 
@@ -134,18 +123,13 @@ const FanTokens = () => {
               </div>
             </div>
           </div>
-        </Col>
-      </Row>
-
-      <Row className="fan-token-row2">
-        <Col sm={6} lg={3}>
           <div className="fan-token-card">
-            <div className="bts-group-container">
-              <img class="bts-group" src={btsPhoto} />
+            <div className="group-image-container">
+              <img class="group-image" src={btsPhoto} />
             </div>
 
-            <div className="bts">
-              <p className="bts-name">BTS</p>
+            <div className="group-name-container">
+              <p className="group-name">BTS</p>
               <img src={btsLogo} />
             </div>
 
@@ -166,16 +150,13 @@ const FanTokens = () => {
               </div>
             </div>
           </div>
-        </Col>
-
-        <Col sm={6} lg={3}>
           <div className="fan-token-card">
-            <div className="bts-group-container">
-              <img class="bts-group" src={btsPhoto} />
+            <div className="group-image-container">
+              <img class="group-image" src={btsPhoto} />
             </div>
 
-            <div className="bts">
-              <p className="bts-name">BTS</p>
+            <div className="group-name-container">
+              <p className="group-name">BTS</p>
               <img src={btsLogo} />
             </div>
 
@@ -196,68 +177,8 @@ const FanTokens = () => {
               </div>
             </div>
           </div>
-        </Col>
-
-        <Col sm={6} lg={3}>
-          <div className="fan-token-card">
-            <div className="bts-group-container">
-              <img class="bts-group" src={btsPhoto} />
-            </div>
-
-            <div className="bts">
-              <p className="bts-name">BTS</p>
-              <img src={btsLogo} />
-            </div>
-
-            <div className="card-content">
-              <div className="card-text">
-                <p>FTO Start Date:</p>
-                <p>TBA</p>
-              </div>
-
-              <div className="card-text">
-                <p>FTO End Date:</p>
-                <p>TBA</p>
-              </div>
-
-              <div className="card-text">
-                <p>Opening Price:</p>
-                <p>TBA</p>
-              </div>
-            </div>
-          </div>
-        </Col>
-
-        <Col sm={6} lg={3}>
-          <div className="fan-token-card">
-            <div className="bts-group-container">
-              <img class="bts-group" src={btsPhoto} />
-            </div>
-
-            <div className="bts">
-              <p className="bts-name">BTS</p>
-              <img src={btsLogo} />
-            </div>
-
-            <div className="card-content">
-              <div className="card-text">
-                <p>FTO Start Date:</p>
-                <p>TBA</p>
-              </div>
-
-              <div className="card-text">
-                <p>FTO End Date:</p>
-                <p>TBA</p>
-              </div>
-
-              <div className="card-text">
-                <p>Opening Price:</p>
-                <p>TBA</p>
-              </div>
-            </div>
-          </div>
-        </Col>
-      </Row>
+        </div>
+      </Container>
     </div>
   );
 };
